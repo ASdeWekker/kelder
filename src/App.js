c40cf06e630c1d82a01d4b0f3d7893f7acc0ec7e
Alex de Wekker <alexdewekker6@gmail.com>
2019-12-01 22:30:23 +0100

    initial commit for new kelder project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react"
+
+class App extends Component {
+	constructor(props) {
+		super()
+		this.value = ""
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<h1>Lorem ipsum</h1>
+			</div>
+		)
+	}
+}
+
+export default App
