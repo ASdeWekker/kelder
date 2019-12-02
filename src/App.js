ec53f8bf03616c0ba5d5f5df5031dd58c94bffed
Alex de Wekker <alexdewekker6@gmail.com>
2019-12-02 23:11:17 +0100

    added font-awesome for icon, did some more styling and started working on the footer buttons

--- src/App.js
+++ src/App.js
@@ -3,13 +3,25 @@ import React, { Component } from "react"
 class App extends Component {
 	constructor(props) {
 		super()
-		this.value = ""
+		this.dateStringOptions = { year: "numeric", month: "long", day: "numeric" }
 	}
 
 	render() {
 		return (
 			<div className="App">
-				<h1>Lorem ipsum</h1>
+				<div className="container">
+					<header className="header">
+						<h1 className="header--title">Kelder</h1>
+						<h3 className="header--date">{
+							new Date().toLocaleDateString("nl-NL", this.dateStringOptions)
+						}</h3>
+					</header>
+				</div>
+				<footer className="footer">
+					<button className="pc-power-button button"><i className="fas fa-power-off"></i></button>
+					<button className="plug-button button"><i className="fas fa-plug"></i></button>
+					<button className="ledstrip-button button"><i className="fas fa-lightbulb"></i></button>
+				</footer>
 			</div>
 		)
 	}
