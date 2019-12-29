2246ffeb14101d96d899ad37c4add641899a98e3
Alex de Wekker <alexdewekker6@gmail.com>
2019-12-29 11:47:56 +0100

    removed old design stuff

--- src/App.js
+++ src/App.js
@@ -9,19 +9,7 @@ class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<div className="container">
-					<header className="header">
-						<h1 className="header--title">Kelder</h1>
-						<h3 className="header--date">{
-							new Date().toLocaleDateString("nl-NL", this.dateStringOptions)
-						}</h3>
-					</header>
-				</div>
-				<footer className="footer">
-					<button className="pc-power-button button"><i className="fas fa-power-off"></i></button>
-					<button className="plug-button button"><i className="fas fa-plug"></i></button>
-					<button className="ledstrip-button button"><i className="fas fa-lightbulb"></i></button>
-				</footer>
+				
 			</div>
 		)
 	}
