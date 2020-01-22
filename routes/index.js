2d591dfbd1a270dadcbff4a4912aba3f0a4b15e3
Alex de Wekker <alexdewekker6@gmail.com>
2020-01-22 21:32:55 +0100

    tried to add the request in the api handler but it looks like a post request in a get request isn't working

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,34 @@ const express = require("express")
 const { exec } = require("child_process")
 const router = express.Router()
 
+function execFunc(command,message) {
+	exec(command, (err, stdout, stderr) => {
+		if (err || stderr) {
+			console.error(`There was an error: ${err}`)
+			return
+		} else {
+			console.log(message)
+		}
+	})
+}
+
 // A route
 router.get("/pc/on", (req, res) => {
-	exec("wol 30:9c:23:04:60:2f", (err, stdout, stderr) => {
+	execFunc("wol 30:9c:23:04:60:2f", "PC has been switched on")
+	res.send("PC has been switched on")
+})
+
+router.get("/pc/off", (req, res) => {
+	res.send("This turns the pc off")
+})
+
+// router.get("led/toggle", (req, res) => {
+// 	execFunc("curl -d 'power=toggle' 192.168.1.220/power", "The ledstrip has been toggled")
+// 	res.send("The ledstrip has been toggled")
+// })
+
+router.get("/led/toggle", (req, res) => {
+	exec("/usr/bin/python /home/alex/dotfiles/scripts/python/ledstrip.py -p toggle", (err, stdout, stderr) => {
 		if (err) {
 			console.error(`exec error: ${err}`)
 			return
@@ -19,10 +44,7 @@ router.get("/pc/on", (req, res) => {
 			res.json({ "message": "The pc has been powered on" })
 		}
 	})
-})
-
-router.get("/pc/off", (req, res) => {
-	res.send("This turns the pc off")
+	res.send("ledstrip toggle")
 })
 
 module.exports = router
