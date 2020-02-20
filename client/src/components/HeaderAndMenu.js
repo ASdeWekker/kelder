a1a79069a60c573c16d224306e3eed9051fc107d
Alex de Wekker <github@dewekker.dev>
2020-02-20 20:00:10 +0100

    did some more menu stuff

--- client/src/components/HeaderAndMenu.js
+++ client/src/components/HeaderAndMenu.js
@@ -3,7 +3,7 @@ import React, { Component } from "react"
 class HeaderAndMenu extends Component {
 	constructor(props) {
 		super()
-		this.dateStringOptions = { month: "long", day: "numeric" }
+		this.dateStringOptions = { month: "long", day: "numeric", hour: "numeric", minute: "numeric", second: "numeric" }
 		this.state = {
 			menuActive: false,
 			date: new Date()
@@ -35,11 +35,11 @@ class HeaderAndMenu extends Component {
 	render() {
 		return (
 			<header className="header">
-				<div className={this.state.menuActive ? "right-menu-active right-menu" : "right-menu"}>
-					<div className={this.state.menuActive ? "right-menu--container right-menu-container-active" : "right-menu--container"}>
+				<div className={this.state.menuActive ? "right-menu right-menu-active" : "right-menu"}>
+					<div className="right-menu--container">
 						<div className="right-menu--container--outside-menu" onClick={this.handleClick}></div>
 						<div className="right-menu--container--menu">
-							<h1 className="right-menu--container--menu--h1" onClick={this.handleClick}>Menutjih</h1>
+							<h1 className="right-menu--container--menu--h1">Menutjih</h1>
 							<h6 className="right-menu--container--menu--version-number">Version:&nbsp;
 								<em>{this.versionNumber}</em>
 							</h6>
@@ -52,9 +52,6 @@ class HeaderAndMenu extends Component {
 							<h1 className="header--container--left--wrapper--text">Kelder</h1>
 							<h2 className="header--container--left--wrapper--date">
 								{this.state.date.toLocaleDateString("nl-NL", this.dateStringOptions)}
-							</h2>
-							<h2 className="header--container--left--wrapper--date">
-
 							</h2>
 						</div>
 					</div>
