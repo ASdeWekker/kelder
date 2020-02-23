2d31dd2bbf9f0d447c5f4768fbedb8a423005dba
Alex de Wekker <github@dewekker.dev>
2020-02-23 19:03:19 +0100

    added a warning when / is accessed

--- kelder-api.js
+++ kelder-api.js
@@ -13,4 +13,11 @@ app.disable("x-powered-by")
 // Call the route(s).
 app.use("/api", indexRouter)
 
+
+// A route for the root, this should not be accessed.
+app.get("/", (req, res) => {
+	res.json({"message": "This part should not be accessed."})
+})
+
+
 module.exports = app
