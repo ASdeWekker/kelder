de1fb56b47e861ae7f0785f11753517d1b84dfcf
Alex de Wekker <github@dewekker.dev>
2020-02-25 21:51:22 +0100

    finally able to change the state of the switch statuses

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -7,40 +7,42 @@ class Header extends Component {
 
 		// Set the state for the date.
 		this.state = {
-			overhead: "OFF",
-			standing: "OFF",
-			amp: "OFF"
+			switchStatus: [
+				{ shortName: "o", longName: "Overhead", status: "OFF" },
+				{ shortName: "s", longName: "Standing", status: "OFF" },
+				{ shortName: "a", longName: "Amp", status: "OFF" },
+			],
+			thing: "lol"
 		}
 		// Set the date options.
 		this.dateStringOptions = { month: "long", day: "numeric"} //, hour: "numeric", minute: "numeric", second: "numeric" }
 		// Bind this to the click functions.
 		this.handleClick = this.handleClick.bind(this)
-		// Get the right-menu and right-menu--container.
+		// Get the right-menu.
 		this.rightMenu = React.createRef()
 	}
 
 	// Lifecycle things for the clock example.
 	// componentDidMount() { this.timerID = setInterval(() => this.tick(), 1000) }
 	// componentWillUnmount() { clearInterval(this.timerID) }
-	// tick() { this.setState({ date: new Date() }) }
 
-	// Check the status of the switches.
+	// Check the status of the switches and set the new state.
 	checkStatus = (dev) => {
-		// let plugsAndShortcodes = {
-		// 	overhead: "o",
-		// 	standing: "s",
-		// 	amp: "a"
-		// }
 		fetch(`/api/wifi/${dev}/status`, {
 			method:"get",
 			dataType:"json",
 			headers: {
 				"Accept": "application/json",
-				"Content-Type": "application/json"
+				"Content-Type": "application/json",
 			}})
 			.then(res => res.json())
 			.then(res => {
-				console.log(res.status)
+				let changedObj = this.state.switchStatus
+				changedObj.map(el => {
+					if (el.shortName === dev) el.status = res.status.toUpperCase()
+					return el
+				})
+				this.setState({ switchStatus: changedObj })
 			})
 	}
 
@@ -57,17 +59,17 @@ class Header extends Component {
 				<div className="header--container">
 					<div className="header--container--left">
 						<p className="header--container--left--status">
-							<span className="left-status" onClick={() => this.checkStatus("s")}>Overhead:</span>
-							<span className="right-status">{this.state.overhead}<br /></span>
-							<span className="left-status">Standing:</span>
-							<span className="right-status">{this.state.standing}<br /></span>
-							<span className="left-status">Amp:</span>
-							<span className="right-status">{this.state.amp}<br /></span>
+							<span className="left-status">{this.state.switchStatus[0].longName}:</span>
+							<span className="right-status">{this.state.switchStatus[0].status}<br /></span>
+							<span className="left-status">{this.state.switchStatus[1].longName}:</span>
+							<span className="right-status">{this.state.switchStatus[1].status}<br /></span>
+							<span className="left-status">{this.state.switchStatus[2].longName}:</span>
+							<span className="right-status">{this.state.switchStatus[2].status}<br /></span>
 						</p>
 					</div>
 					<div className="header--container--right">
 						<div className="header--container--right--wrapper">
-							<h1 className="header--container--right--wrapper--text">Kelder<br />
+							<h1 className="header--container--right--wrapper--text" onClick={() => this.checkStatus("s")}>Kelder<br />
 								<span className="header--container--right--wrapper--text--date">
 									{new Date().toLocaleDateString("nl-NL", this.dateStringOptions)}
 								</span>
