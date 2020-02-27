d462863faac85f7922dfc14c68e71ef5022c0b98
Alex de Wekker <github@dewekker.dev>
2020-02-27 22:25:13 +0100

    added functionality to slide the hcmenu in

--- client/src/components/HomeControl.js
+++ client/src/components/HomeControl.js
@@ -2,8 +2,19 @@ import React, { Component } from "react"
 import HcMenu from "./HcMenu"
 
 class HomeControl extends Component {
+	constructor(props) {
+		super()
+
+		this.state = {
+			// State to show if the menu is active.
+			isActive: false,
+			// State for the menu header name.
+			menuName: "Wifi plugs",
+		}
+	}
+
 	// A function to fetch the different API endpoints.
-	apiCall = (endpoint) => {
+	apiCall = endpoint => {
 		fetch(`/api/${endpoint}`, {
 			method:"get",
 			dataType:"json",
@@ -15,6 +26,22 @@ class HomeControl extends Component {
 			.then(res => console.log(res))
 	}
 
+	// A function to render the menu.
+	menu = () => {
+		if (this.state.isActive) {
+			return <HcMenu options={this.props} />
+		} else {
+			return
+		}
+	}
+	
+	// A function to open the options menu with the right props.
+	openMenu = () => {
+		this.setState({
+			isActive: !this.state.isActive,
+		})
+	}
+
 	render() {
 		return (
 			<div className="home-control">
@@ -23,7 +50,7 @@ class HomeControl extends Component {
 						<button className="container--button--icon" onClick={() => this.apiCall("wifi/A/toggle")}>
 							<i className="fas fa-power-off"></i>
 						</button>
-						<button className="container--button--text">Plugs</button>
+						<button className="container--button--text" onClick={this.openMenu}>Plugs</button>
 					</div>
 					<div id="ledstrip" className="container--button">
 						<button className="container--button--icon" onClick={() => this.apiCall("led/p/toggle")}>
@@ -38,6 +65,7 @@ class HomeControl extends Component {
 						<button className="container--button--text" onClick={() => this.apiCall("pc/on")}>PC</button>
 					</div>
 				</div>
+				{this.menu()}
 			</div>
 		)
 	}
