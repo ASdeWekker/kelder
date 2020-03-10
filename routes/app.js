b86ffc7a8fe7b36d79ca2816af1d9d5f04fff73d
Alex de Wekker <github@dewekker.dev>
2020-03-10 23:03:18 +0100

    added log4js

--- routes/app.js
+++ routes/app.js
@@ -1,9 +1,36 @@
 const express = require("express")
 const { Client } = require("pg")
+const log4js = require("log4js")
 const connStr = require("../config/connection").connectionString
 const queries = require("../config/queries")
 
+// Declare the router and log4js.
 const router = express.Router()
+const logger = log4js.getLogger("activity")
+
+// Log4js configuration.
+log4js.configure({
+	appenders: {
+		file: {
+			type: "file",
+			filename: "./logs/activity.log",
+			maxLogSize: 10 * 1024 * 1024, // = 10 Mb
+			backups: 5, // Keep 5 backups
+			compress: true,
+			encoding: "utf-8",
+			mode: 0o0640,
+			flags: "w+"
+		},
+		dateFile: {
+			type: "dateFile",
+			filename: "./logs/important-activity.log",
+			pattern: "yyyy-MM-dd-hh",
+			compress: true
+		},
+		out: { type: "stdout" }
+	},
+	categories: { default: { appenders: ["file", "dateFile", "out"], level: "trace" } }
+})
 
 const client = new Client({ connectionString: connStr })
 client.connect()
@@ -22,7 +49,10 @@ router.get("/practice", (req, res) => {
 			console.log(`Fetched ${data.rows.length} rows`)
 			res.json(data.rows)
 		})
-		.catch(e => console.error(e.stack))
+		.catch(e => {
+			console.error(e.stack)
+			logger.debug("wrong")
+		})
 })
 router.get("/projects", (req, res) => {
 	client.query(queries.getProjects)
