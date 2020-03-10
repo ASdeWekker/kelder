26c3570b530857601029589fc0309202a97b2bbe
Alex de Wekker <github@dewekker.dev>
2020-03-10 22:52:02 +0100

    changed back to the PSQLIP environment variable

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 // Connect to the database server.
 
 // Connection URI.
-const connStr = `postgresql://${process.env.PSQLU}:${process.env.PSQLW}@piet.local:${process.env.PSQLPORT}/dekelder`
+const connStr = `postgresql://${process.env.PSQLU}:${process.env.PSQLW}@${process.env.PSQLIP}:${process.env.PSQLPORT}/dekelder`
 
 module.exports = {
 	connectionString: connStr,
